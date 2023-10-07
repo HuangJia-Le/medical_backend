9cf174e87f37eb8d4a4f963bad88ffdbb05ae75f
monster <1062004566@qq.com>
2023-10-07 10:46:27 +0800

    first commit

---
+++ src/main/java/com/example/medical_backend/controller/FileController.java
@@ -0,0 +1,62 @@
+package com.example.medical_backend.controller;
+
+
+import cn.hutool.core.io.FileUtil;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+@RestController
+@RequestMapping("/file")
+public class FileController {
+
+    @Value("${files.upload.path}")
+    private String fileUploadPath;
+
+    @Value("${server.ip}")
+    private String serverIp;
+
+    /**
+     * 文件上传接口
+     * @param file 前端传递过来的文件
+     * @return
+     * @throws IOException
+     */
+    @PostMapping("/upload")
+    public String upload(@RequestParam MultipartFile file) throws IOException {
+        String originalFilename = file.getOriginalFilename();//获取文件名
+        File uploadFile = new File(fileUploadPath + originalFilename);
+        // 判断配置的文件目录是否存在，若不存在则创建一个新的文件目录
+        File parentFile = uploadFile.getParentFile();
+        if(!parentFile.exists()) {
+            parentFile.mkdirs();
+        }
+        file.transferTo(uploadFile);
+        return file.getOriginalFilename();
+    }
+
+    /**
+     * 文件下载接口
+     */
+    @GetMapping("/download/{fileName}")
+    public void download(@PathVariable String fileName, HttpServletResponse response) throws IOException {
+        // 根据文件的唯一标识码获取文件
+        File uploadFile = new File(fileUploadPath + fileName);
+        // 设置输出流的格式
+        ServletOutputStream os = response.getOutputStream();
+        response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, StandardCharsets.UTF_8));
+        response.setContentType("application/octet-stream");
+
+        // 读取文件的字节流
+        os.write(FileUtil.readBytes(uploadFile));
+        os.flush();
+        os.close();
+    }
+}
